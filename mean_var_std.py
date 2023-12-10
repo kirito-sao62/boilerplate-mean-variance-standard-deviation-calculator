993724ecfbfbfa9dedbaafc9f8ec4ba78b6a36ed
kirito-sao62 <119740205+kirito-sao62@users.noreply.github.com>
2023-12-10 22:50:43 +0530

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,15 @@
 import numpy as np
 
-def calculate(list):
-
-
-
-
-    return calculations
+def calculate(a):
+    try:
+        b = a.reshape(3,3)
+        data = {}
+        data["mean"] = [b.mean(axis=0), b.mean(axis=1), a.mean()]
+        data["variance"] = [b.var(axis=0), b.var(axis=1), a.var()]
+        data["standard deviation"] = [b.std(axis=0), b.std(axis=1), a.std()]
+        data["max"] = [b.max(axis=0), b.max(axis=1), a.max()]
+        data["min"] = [b.min(axis=0), b.min(axis=1), a.min()]
+        data["sum"] = [b.sum(axis=0), b.sum(axis=1), a.sum()]
+    except ValueError:
+        return "List must contains nine numbers."
+    return data
